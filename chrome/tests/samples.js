88a925ac72f42cc068d911ad07e4bfb21a927740
trockerapp <trockerapp@gmail.com>
2018-11-23 16:06:43 -0800

    adds tests for url matching

---
+++ chrome/tests/samples.js
@@ -0,0 +1,179 @@
+function getOpenTrackerSamples(){
+	var openTrackers = [
+		{
+			"name": "YW", 
+   "samples":{
+    "hit": ["https://t.yesware.com/t/elriutdmnvx23vb/lkwugfsndbv/spacer.gif"], 
+    "miss": []
+   }
+		},
+		{
+			"name": "SK", 
+			"samples":{
+    "hit": ["http://t.sidekickopen04.com/e1t/o/5/DFKJDKFJSDWPOE&amp;pi=sajkdhsjfhdg23s"], 
+    "miss": ["https://www.hubspot.com"]
+   }
+		},
+		{
+			"name": "MT", 
+			"samples":{
+    "hit": ["https://mailtrack.io/trace/mail/50743568273456345vp827439f.png"], 
+    "miss": ["https://mailtrack.io/en/"]
+   }
+		},
+		{
+			"name": "MC",
+			"samples":{
+    "hit": ["http://mandrillapp.com/track/open.php?u=48576348756348756&amp;id=27364827634bdshdghgsd82739827"], 
+    "miss": []
+   }
+		},
+		{
+			"name": "SG",
+			"samples":{
+    "hit": ["https://u932479834.ct.sendgrid.net/wf/open?upn=SKJHROITBASOIWe87434DSIUDFSMNCSIUDWRE-EuyisouyfsfWRIUWRMVNS4348783"], 
+    "miss": []
+   }
+		},
+		{
+			"name": "IC",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "BT",
+			"samples":{
+    "hit": ["http://s.bl-1.com/jdhskdMF45.gif"], 
+    "miss": []
+   }
+		},
+		{
+			"name": "MM",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "MTA",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "CC",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "GA",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "IB",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "MP",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "GN",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		}
+	];
+	return openTrackers;
+}
+
+function getClickTrackerSamples(){
+	var clickTrackers = [
+		{
+			"name": "YW", 
+			"samples":{
+    "hit": ["http://t.yesware.com/tl/djhskdjshduew8e7lkdds?ytl=http://example.com"], 
+    "miss": []
+   }
+		},
+		{
+			"name": "SK", 
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "MC",
+			"samples":{
+    "hit": ["http://toutapp.us1.list-manage.com/track/click?u=erdinbosfh87343&id=jhasjka7246&e=kjdsd7263"], 
+    "miss": []
+   }
+		},
+		{
+			"name": "SG",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "IC",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "BT",
+			"samples":{
+    "hit": ["http://s.bl-1.com/h/uyre73642sdhF?url=http://www.example.com/"], 
+    "miss": []
+   }
+		},
+		{
+			"name": "MM",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "MTA",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+		{
+			"name": "IB",
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		},
+  {
+			"name": "SL", 
+			"samples":{
+    "hit": [], 
+    "miss": []
+   }
+		}
+	];
+	return clickTrackers;
+}
